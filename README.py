d19fad44c3ab9888f6c9f267594e26b4717bdc28
bman7903 <bman7903@gmail.com>
2016-03-14 23:44:22 +0000

    Update README.py

--- README.py
+++ README.py
@@ -1,8 +1,5 @@
 You Tube Search Client.
 
-Essentially this will search for X pages of resaults.  Using the image previes name as a key it then downloads the icons, embedding the track info into the image while in ram using python pil library.  You then have the choice to download or stream using youtube-dl mpv or vlc.  The image downloading and batch conversion is multithreaded. Should be cross platform.
-
-ToDo: make coin-noise on dl-complete
-
+Client searchs for X pages.  Using the image preview as a key, it downloads the icons, embedding the track info into the image while in ram using python pil library.  You then have the choice to download or stream using youtube-dl mpv or vlc.  The image downloading and batch conversion is multithreaded. Should be cross platform.
 
 Example:  python tubless.py justin beiber
